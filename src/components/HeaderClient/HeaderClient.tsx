5df0c43dc62d82e0226241136e5fa251feee193d
leandro <leandro_antunes28@hotmail.com>
2024-08-18 00:25:23 -0300

    import useefect faltando

--- src/components/HeaderClient/HeaderClient.tsx
+++ src/components/HeaderClient/HeaderClient.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import logoImg from "../../assets/images/logo-hanami.jpg";
 import './style.css';
 import { Link } from "react-router-dom";
